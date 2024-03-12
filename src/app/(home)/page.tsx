475e3bdba9cf6ef09b293284e9ca6014942dfc1a
kimyziii <yeaji105@naver.com>
2024-03-13 04:00:38 +0900

    design: homepage width, marginTop change

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,12 @@
 'use client'
-import HomeToday from '@/components/home-today'
-import HomeTomorrow from '@/components/home-tomorrow'
+import HomeToday from '@/app/(home)/components/home-today'
+import HomeTomorrow from '@/app/(home)/components/home-tomorrow'
+import React from 'react'
 
-const HomePage = () => {
+const HomePage: React.FC = () => {
   return (
-    <div className='w-full h-[100vh] bg-background flex flex-row justify-center items-center'>
-      <div className='desktop:w-[1476px] laptop:w-[1220px] tablet:w-[964px] mobile:w-[580px] h-[80vh] grid grid-cols-3 place-items-center gap-4'>
+    <div className='w-full h-[80vh] bg-background flex flex-row justify-center mt-[10vh]'>
+      <div className='w-[90%] grid grid-cols-3 place-items-center gap-4'>
         <HomeToday />
         <HomeTomorrow />
       </div>
