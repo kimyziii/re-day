d7c7730f3c8bffdc1d10430dbe607336cb730df3
kimyziii <yeaji105@naver.com>
2024-03-28 03:58:53 +0900

    feat: navbar

--- src/layouts/nav.tsx
+++ src/layouts/nav.tsx
@@ -1,7 +1,21 @@
+'use client'
 import UserContext from '@/app/(shared)/context/userContext'
 import { IUser } from '@/app/models/user'
 import { useSession } from 'next-auth/react'
+import { usePathname, useRouter } from 'next/navigation'
 import { useContext, useEffect } from 'react'
+import {
+  AiOutlineHome,
+  AiOutlineSearch,
+  AiOutlineUnorderedList,
+  AiFillSetting,
+} from 'react-icons/ai'
+
+const NavBar = () => {
+  const router = useRouter()
+  const pathname = usePathname()
+  const returnNullArr = ['/login', '/signup']
+
   const { data: session } = useSession()
   const { setUser } = useContext(UserContext)
   useEffect(() => {
@@ -17,3 +31,31 @@ import { useContext, useEffect } from 'react'
       router.push('/login')
     }
   }, [session])
+
+  if (returnNullArr.includes(pathname)) return null
+
+  return (
+    <div className='flex flex-col justify-between items-center w-[60px] h-[100vh] bg-secondary-foreground text-white px-3 py-6'>
+      <div className='space-y-6'>
+        <AiOutlineHome
+          size={22}
+          color='white'
+          className='cursor-pointer'
+          onClick={() => router.push('/')}
+        />
+        <AiOutlineSearch size={22} color='white' />
+        <AiOutlineUnorderedList size={22} color='white' />
+      </div>
+      <div>
+        <AiFillSetting
+          size={22}
+          color='white'
+          className='cursor-pointer'
+          onClick={() => router.push(`/settings`)}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
